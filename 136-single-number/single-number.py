ab88d636a2be1cdf18671cf94e0ae67a88bb49a9
Ruthud23 <ruthudev2003@gmail.com>
2026-09-13 22:55:04 +0530

    Time: 7 ms (29.42%) | Memory: 21.7 MB (10.67%) - LeetSync

---
+++ 136-single-number/single-number.py
@@ -0,0 +1,12 @@
+class Solution:
+    def singleNumber(self, nums):
+        seen = set()
+
+        for num in nums:
+            if num in seen:
+                seen.remove(num)
+            else:
+                seen.add(num)
+
+        return seen.pop()
+        
